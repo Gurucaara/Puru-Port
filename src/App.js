2e5e4856c32428ba43b6d8a62e39594ffe072de7
Puru-Rana <pururana24@gmail.com>
2023-07-03 09:16:31 -0400

    did the main attraction page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 const App = () => {
     return (
         <div className="text-sm">
             <NavBar/>
+            <Home/>
         </div>
     )
 }
