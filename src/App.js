873b072870f36ecf68b3fc030f4171c89ce66f23
Puru-Rana <pururana24@gmail.com>
2023-07-03 13:21:40 -0400

    done with the experience comp

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
 const App = () => {
     return (
         <div className="text-sm">
@@ -11,6 +12,7 @@ const App = () => {
             <Home/>
             <About/>
             <Portfolio/>
+            <Experience/>
 
             <SocialLinks/>
             
