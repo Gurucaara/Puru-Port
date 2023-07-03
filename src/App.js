b6da9df454742068e07df2d0fd08f0eaf7c3bd5f
Puru-Rana <pururana24@gmail.com>
2023-07-03 11:55:47 -0400

    done with the about section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
 const App = () => {
     return (
         <div className="text-sm">
             <NavBar/>
             <Home/>
+            <About/>
 
             <SocialLinks/>
+            
+
         </div>
     )
 }
