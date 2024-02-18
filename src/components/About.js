8c154e6f61fa0fbf42fdeeb4dc5c11e34e0c1937
Puru-Rana <pururana24@gmail.com>
2024-02-17 22:41:35 -0500

    6: Imp Home & About

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,42 @@
 import React from "react";
 const About = () => {
-    return (
-        <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white ">
-            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-                <div className="pb-8">
-                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-                        About
-                    </p>
-                </div>
-                <p className="text-xl mt-20">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                </p>
-
-                <br/>
-
-                <p className="text-xl">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  
-                </p>
-            </div>
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white "
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
-    )
-}
+        <p className="text-xl mt-20">
+          My name is Puru Rana, a passionate web developer based in London,
+          Canada. With a knack for both web and mobile application development,
+          driven by my passion for innovation. I'm committed to pushing
+          boundaries and surpassing expectations with each project I undertake.
+          Keeping a keen eye on industry trends, I'm always on the lookout for
+          emerging technologies and techniques to incorporate into my work,
+          ensuring that I deliver solutions that are creative.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          Beyond just coding, I believe in the power of collaboration and
+          communication. Understanding your vision and goals is paramount to me,
+          and I'm dedicated to working closely with you every step of the way to
+          bring that vision to life.
+        </p>
+        <p className="text-xl">
+          So, let's embark on this journey together. Let's brainstorm, create,
+          and innovate. Let's turn your ideas into captivating digital
+          experiences that shine. Get in touch, and let's make magic happen.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,67 @@
 import React from "react";
 import heroImage from "../assets/heroImage.png";
-import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { MdOutlineKeyboardArrowRight, MdOutlineLink } from "react-icons/md";
 import { Link } from "react-scroll";
-const Home = () => {
-    return (
-      <div
-        name="home"
-        className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
-      >
-        <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-          <div className="flex flex-col justify-center h-full">
-            <h2 className="text-4xl sm:text-7xl font-bold text-white">
-              I'm a Full Stack Developer
-            </h2>
-            <p className="text-gray-500 py-4 max-w-md">
-              I have 3 months of experience building and desgining software.
-              Currently, I love to work on web application using technologies
-              like React, Tailwind, and Next JS .
-            </p>
+import { Button } from "@mui/material";
 
-            <div >
-              <Link to="portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-green-400 to-green-800">
-              Portfolio
-              <span className="group-hover:rotate-90 duration-300">
-                <MdOutlineKeyboardArrowRight size={20} className="ml-1" />
-              </span>
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I'm a Full Stack Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            Crafting engaging web applications using React, Next.js, and
+            Tailwind CSS, while continuously expanding skills with technologies
+            like GraphQL, Prisma, and beyond.
+          </p>
+
+          <div className="flex">
+            <div className="mr-4">
+              <Link
+                to="portfolio"
+                smooth
+                duration={500}
+                className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-green-400 to-green-800"
+              >
+                Portfolio
+                <span className="group-hover:rotate-90 duration-300">
+                  <MdOutlineKeyboardArrowRight size={20} className="ml-1" />
+                </span>
               </Link>
             </div>
-          </div>
-
-          <div>
-            <img
-              src={heroImage}
-              alt="my profile"
-              className="rounded-3xl mx-auto w-2/3 md:w-full "
-            />
+            <div className="mt-2">
+              <Button
+                href="https://puru-blogs.vercel.app"
+                variant="contained"
+                target="_blank"
+                size="large"
+                style={{ textTransform: "none" }}
+                className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-purple-400 to-purple-800 "
+              >
+                Blog
+                <span className="group-hover:translate-x-4 duration-300">
+                  <MdOutlineLink size={20} className="ml-1" />
+                </span>
+              </Button>
+            </div>
           </div>
         </div>
+
+        <div>
+          <img
+            src={heroImage}
+            alt="my profile"
+            className="rounded-3xl mx-auto w-2/3 md:w-full "
+          />
+        </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 export default Home;
